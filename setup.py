92e358193e88344c402c84918cc1c4facf40ba46
Sumedh Bhatwadekar <sumedhbhatwadekar122@gmail.com>
2025-05-31 14:54:09 +0530

    after adding

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+def get_requirements() ->list[str]:
+    requirements_list = list[str] = []
+    return requirements_list
+
+setup (
+    name = 'sensor',
+    version = "0.0.1",
+    author = "Sumedh",
+    author_email = "sumedhbhatwadekar122@gmail.com",
+    packages = find_packages(),
+    install_requires = ["pymongo"],
+)
